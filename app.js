a41e854debcdc0281331f961c92e69a5040ccc22
Desfint <allesarfint@gmail.com>
2021-07-20 00:07:26 -0500

    Basic app structure.

---
+++ app.js
@@ -0,0 +1,15 @@
+/* 
+Problem:
+    Make a sketch app where the user can draw when hovering the mouse over it.
+    Give the user the ability to change the resolution of the grid and clear
+    what has been drawn.
+
+Plan: 
+    - User interface: The browser window.
+    - Data inputs: Hovering the mouse over the sketching area to draw on it.
+        How big the resolution of the sketching area the user wants it to be.
+    - Desired outputs: The area where the user hover the mouse over change of
+        color. The resolution of the sketching area change to what the user
+        desires.
+*/
+
